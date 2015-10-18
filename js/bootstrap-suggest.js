2582ccd6105fa0f31953fe757c1f85dda4827706
avipinto <pinto.avi@gmail.com>
2015-10-18 11:18:08 +0300

    on IE8 \r\n counts as two characters which caused problems in identifying the @ that is not on the first line of the textarea

--- js/bootstrap-suggest.js
+++ js/bootstrap-suggest.js
@@ -342,25 +342,28 @@
 
 			this.hide();
 		},
-		__getSelection: function(el)
+		__getSelection: function (el)
 		{
 		    var start = 0,
 		        end = 0,
+                rawValue,
 		        normalizedValue,
 		        range,
 		        textInputRange,
 		        len,
 		        endRange;
-
 		    if (typeof el.selectionStart == "number" && typeof el.selectionEnd == "number") {
 		        start = el.selectionStart;
 		        end = el.selectionEnd;
 		    } else {
+		        el.focus();
+
 		        range = document.selection.createRange();
 
-		        if (range && range.parentElement() == el) {
-		            len = el.value.length;
-		            normalizedValue = el.value.replace(/\r\n/g, "\n");
+		        if (range && range.parentElement() === el) {
+		            rawValue = el.value;
+		            len = rawValue.length;
+		            normalizedValue = rawValue.replace(/\r\n/g, "\n");
 
 		            // Create a working TextRange that lives only in the input
 		            textInputRange = el.createTextRange();
@@ -385,9 +388,13 @@
 		                    end += normalizedValue.slice(0, end).split("\n").length - 1;
 		                }
 		            }
+
+		            /// normalize newlines
+		            start -= (rawValue.substring(0, start).split('\r\n').length - 1);
+		            end -= (rawValue.substring(0, end).split('\r\n').length - 1);
+		            /// normalize newlines
 		        }
 		    }
-
 		    return {
 		        start: start,
 		        end: end
